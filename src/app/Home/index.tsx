372160421aa7fa6f1c7cee5f1c0d5f228b829464
dhoouglas <dhoouglas@hotmail.com>
2025-06-03 13:37:26 -0300

    feat: created Filter and StatusIcon component

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -2,14 +2,23 @@ import { Image, View } from "react-native";
 import { styles } from "./styles";
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
+import { Filter } from "@/components/Filter";
+import { FilterStatus } from "@/@types/FilterStatus";
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Image style={styles.logo} source={require("@/assets/logo.png")} />
 
-      <Input placeholder="O que você precisa comprar ?" />
-      <Button title="Adicionar" />
+      <View style={styles.form}>
+        <Input placeholder="O que você precisa comprar ?" />
+        <Button title="Adicionar" />
+      </View>
+
+      <View style={styles.content}>
+        <Filter status={FilterStatus.DONE} isActive />
+        <Filter status={FilterStatus.PENDING} isActive={false} />
+      </View>
     </View>
   );
 }
