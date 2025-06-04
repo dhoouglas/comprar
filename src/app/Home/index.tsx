4bab3008765b13ee7b9ab561caf6e9eefbdada6e
dhoouglas <dhoouglas@hotmail.com>
2025-06-04 12:07:58 -0300

    feat: using asyncStorage and states

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,4 +1,12 @@
-import { Image, View, TouchableOpacity, Text, FlatList } from "react-native";
+import { useState } from "react";
+import {
+  Image,
+  View,
+  TouchableOpacity,
+  Text,
+  FlatList,
+  Alert,
+} from "react-native";
 
 import { styles } from "./styles";
 import { Button } from "@/components/Button";
@@ -8,32 +16,45 @@ import { FilterStatus } from "@/@types/FilterStatus";
 import { Item } from "@/components/Item";
 
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE];
-const ITEMS = [
-  { id: "1", status: FilterStatus.DONE, description: "1 pacote de café" },
-  {
-    id: "2",
-    status: FilterStatus.PENDING,
-    description: "3 pacotes de macarrão",
-  },
-  { id: "3", status: FilterStatus.PENDING, description: "3 cebolas" },
-];
 
 export function Home() {
-  console.log("ITEMS", ITEMS);
+  const [filter, setFilter] = useState(FilterStatus.PENDING);
+  const [description, setDescription] = useState("");
+  const [items, setItems] = useState<any>([]);
+
+  function handleAddItem() {
+    if (!description.trim()) {
+      return Alert.alert("Adicionar", "Informe a descrição para adicionar");
+    }
+
+    const newItem = {
+      id: Math.random().toString(36).substring(2),
+      description,
+      status: FilterStatus.PENDING,
+    };
+  }
 
   return (
     <View style={styles.container}>
       <Image style={styles.logo} source={require("@/assets/logo.png")} />
 
       <View style={styles.form}>
-        <Input placeholder="O que você precisa comprar ?" />
-        <Button title="Adicionar" />
+        <Input
+          placeholder="O que você precisa comprar ?"
+          onChangeText={setDescription}
+        />
+        <Button title="Adicionar" onPress={handleAddItem} />
       </View>
 
       <View style={styles.content}>
         <View style={styles.header}>
           {FILTER_STATUS.map((status) => (
-            <Filter key={status} status={status} isActive />
+            <Filter
+              key={status}
+              status={status}
+              isActive={filter === status}
+              onPress={() => setFilter(status)}
+            />
           ))}
 
           <TouchableOpacity style={styles.clearButton}>
@@ -42,8 +63,8 @@ export function Home() {
         </View>
 
         <FlatList
-          data={ITEMS}
-          keyExtractor={(item) => item.id}
+          data={items}
+          keyExtractor={(item) => item}
           renderItem={({ item }) => (
             <Item
               data={item}
